9c78f85d49080b414179c805b4dd8a4836fe75f5
Juliana Zambon <64793722+JulianaZambon@users.noreply.github.com>
2024-07-11 15:36:18 -0300

    Update lista.c

--- tp4/lista.c
+++ tp4/lista.c
@@ -51,10 +51,75 @@ int lista_insere_inicio(struct lista *lista, int chave)
 
 int lista_insere_fim(struct lista *lista, int chave)
 {
+    struct nodo *novo;                                 // cria um novo nodo
+    novo = (struct nodo *)malloc(sizeof(struct nodo)); // aloca memoria para o novo nodo
+
+    // em caso de erro de alocação
+    if (novo == NULL)
+        return 0;
+
+    novo->chave = chave; // novo nodo recebe a chave
+    novo->prox = NULL;   // novo nodo aponta para NULL
+
+    // se a lista estiver vazia
+    if (lista->ini == NULL)
+    {
+        lista->ini = novo; // inicio da lista recebe o novo nodo
+    }
+    else
+    {
+        struct nodo *aux; // cria um nodo auxiliar
+        aux = lista->ini; // auxiliar recebe o inicio da lista
+
+        // percorre a lista ate o final
+        while (aux->prox != NULL)
+        {
+            aux = aux->prox;
+        }
+
+        aux->prox = novo; // o ultimo nodo aponta para o novo nodo
+    }
+
+    lista->tamanho++; // incrementa o tamanho da lista
+
+    return 1;
 }
 
 int lista_insere_ordenado(struct lista *lista, int chave)
 {
+    struct nodo *novo;                                 // cria um novo nodo
+    novo = (struct nodo *)malloc(sizeof(struct nodo)); // aloca memoria para o novo nodo
+
+    // em caso de erro de alocação
+    if (novo == NULL)
+        return 0;
+
+    novo->chave = chave; // novo nodo recebe a chave
+
+    // se a lista estiver vazia ou a chave for menor que o inicio da lista
+    if (lista->ini == NULL || chave < lista->ini->chave)
+    {
+        novo->prox = lista->ini; // novo nodo aponta para o inicio da lista
+        lista->ini = novo;       // inicio da lista recebe o novo nodo
+    }
+    else
+    {
+        struct nodo *aux; // cria um nodo auxiliar
+        aux = lista->ini; // auxiliar recebe o inicio da lista
+
+        // percorre a lista ate o final ou ate a chave ser maior que o nodo
+        while (aux->prox != NULL && chave > aux->prox->chave)
+        {
+            aux = aux->prox;
+        }
+
+        novo->prox = aux->prox; // novo nodo aponta para o proximo nodo
+        aux->prox = novo;       // nodo anterior aponta para o novo nodo
+    }
+
+    lista->tamanho++; // incrementa o tamanho da lista
+
+    return 1;
 }
 
 int lista_remove_inicio(struct lista *lista, int *chave)
@@ -62,8 +127,15 @@ int lista_remove_inicio(struct lista *lista, int *chave)
     if (lista->ini == NULL)
         return 0;
 
-    // Remove o elemento do inicio da lista e o retorna
-    // no parametro chave. Nao confundir com o retorno da funcao.
+    struct nodo *aux; // cria um nodo auxiliar
+    aux = lista->ini; // auxiliar recebe o inicio da lista
+    *chave = aux->chave; // chave recebe a chave do nodo
+
+    lista->ini = lista->ini->prox; // inicio da lista recebe o proximo nodo
+    free(aux); // libera a memoria do nodo
+    lista->tamanho--; // decrementa o tamanho da lista
+
+    return 1;
 }
 
 int lista_remove_fim(struct lista *lista, int *chave)
@@ -71,8 +143,32 @@ int lista_remove_fim(struct lista *lista, int *chave)
     if (lista->ini == NULL)
         return 0;
 
-    // Remove o elemento do final da lista e o retorna
-    // no parametro chave. Nao confundir com o retorno da funcao.
+    struct nodo *aux; // cria um nodo auxiliar
+    aux = lista->ini; // auxiliar recebe o inicio da lista
+
+    // se a lista tiver apenas um nodo
+    if (aux->prox == NULL)
+    {
+        *chave = aux->chave; // chave recebe a chave do nodo
+        lista->ini = NULL; // inicio da lista recebe NULL
+        free(aux); // libera a memoria do nodo
+    }
+    else
+    {
+        // percorre a lista ate o penultimo nodo
+        while (aux->prox->prox != NULL)
+        {
+            aux = aux->prox;
+        }
+
+        *chave = aux->prox->chave; // chave recebe a chave do nodo
+        free(aux->prox); // libera a memoria do nodo
+        aux->prox = NULL; // penultimo nodo aponta para NULL
+    }
+
+    lista->tamanho--; // decrementa o tamanho da lista
+
+    return 1;
 }
 
 int lista_remove_ordenado(struct lista *lista, int chave)
@@ -80,7 +176,34 @@ int lista_remove_ordenado(struct lista *lista, int chave)
     if (lista->ini == NULL)
         return 0;
 
-    // Remove o elemento chave da lista mantendo-a ordenada.
+    struct nodo *aux; // cria um nodo auxiliar
+    aux = lista->ini; // auxiliar recebe o inicio da lista
+
+    // se a chave for igual ao inicio da lista
+    if (aux->chave == chave)
+    {
+        lista->ini = lista->ini->prox; // inicio da lista recebe o proximo nodo
+        free(aux); // libera a memoria do nodo
+        lista->tamanho--; // decrementa o tamanho da lista
+        return 1;
+    }
+
+    // percorre a lista ate o penultimo nodo
+    while (aux->prox != NULL)
+    {
+        if (aux->prox->chave == chave)
+        {
+            struct nodo *aux2; // cria um nodo auxiliar
+            aux2 = aux->prox; // auxiliar recebe o proximo nodo
+            aux->prox = aux->prox->prox; // nodo anterior aponta para o proximo nodo
+            free(aux2); // libera a memoria do nodo
+            lista->tamanho--; // decrementa o tamanho da lista
+            return 1;
+        }
+        aux = aux->prox; // auxiliar recebe o proximo nodo
+    }
+
+    return 0;
 }
 
 int lista_vazia(struct lista *lista)
@@ -104,10 +227,35 @@ int lista_pertence(struct lista *lista, int chave)
         return 0;
 }
 
+/*
+ * As funcoes abaixo implementam um iterador que vao permitir o usuario
+ * de uma lista percorre-la, sem conhecer sua estrutura.
+ *
+ * Em conjunto, elas simulam um laço, sendo a inicializacao deste
+ * a funcao lista_inicia_iterador e o incremento sendo a funcao
+ * lista_incrementa_iterador.
+ *
+ * O ponteiro ptr da struct (iterador) eh inicializado apontando
+ * para o primeiro elemento e incrementado ate' o ultimo elemento 
+ * da lista.
+ *
+ * Ver exemplo de uso em testa_lista.c (funcao testa_imprimir_lista)
+*/
+
+/*
+ * Inicializa ptr usado na funcao incrementa_iterador.
+ * A funcao main deve garantir que a lista nao eh vazia.
+*/
 void lista_inicia_iterador(struct lista *lista)
 {
+    
 }
 
+/*
+ * Devolve no parametro *chave o elemento apontado e incrementa o iterador.
+ * A funcao retorna 0 caso o iterador ultrapasse o ultimo elemento, ou retorna
+ * 1 caso o iterador aponte para um elemento valido (dentro da lista).
+*/
 int lista_incrementa_iterador(struct lista *lista, int *chave)
 {
 }
